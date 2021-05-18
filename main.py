f49a355a641c7940263776e859a9a3d58b41ae2f
Moussa Cissokho <cissokhomoussa08@outlook.fr>
2021-05-18 21:06:54 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+# class gridlayoutExample(gridlayout):
+#    pass
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-bt"
+        for i in range(0, 100):
+            #size = dp(100)+i*10
+            size = dp(100)
+            btn = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
+            self.add_widget(btn)
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+class BoxLayoutExample(BoxLayout):
+    pass
+"""    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation="vertical"
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        b3 = Button(text="C")
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3) """
+
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App):
+    pass
+
+TheLabApp().run()
+
